6331bb22a765e6414fcdbd53231e3bec71693bda
Marcos Miranda <92108541+MarcosMirandaDam@users.noreply.github.com>
2024-04-13 11:35:14 +0200

    Add files via upload

---
+++ TypeScript/funcionSubirSalarioEmpleado.js
@@ -0,0 +1,73 @@
+var empleados2 = [
+    {
+        nombre: "Abel",
+        edad: 22,
+        salario: 1000,
+        direccion: {
+            calle: "Sta Rita",
+            numero: 3,
+            ciudad: "La Felguera"
+        }
+    },
+    {
+        nombre: "Ana",
+        edad: 24,
+        salario: 1100,
+        direccion: {
+            calle: "La Rue",
+            numero: 32,
+            ciudad: "Oviedo"
+        }
+    },
+    {
+        nombre: "Bartolomé",
+        edad: 32,
+        salario: 2000,
+        direccion: {
+            calle: "Uria",
+            numero: 4,
+            ciudad: "Oviedo"
+        }
+    },
+    {
+        nombre: "Bárbara",
+        edad: 42,
+        salario: 1800,
+        direccion: {
+            calle: "La nueva",
+            numero: 1,
+            ciudad: "Gijón"
+        }
+    },
+    {
+        nombre: "Carlos",
+        edad: 34,
+        salario: 1000,
+        direccion: {
+            calle: "El Percebe",
+            numero: 9,
+            ciudad: "La Felguera"
+        }
+    },
+    {
+        nombre: "Cristina",
+        edad: 54,
+        salario: 2100,
+        direccion: {
+            calle: "San Agustín",
+            numero: 12,
+            ciudad: "Gijón"
+        }
+    }
+];
+var subidaAplicar = 10;
+function subirSalario(empleados2) {
+    return empleados2.map(function (empleado) {
+        empleado.salario += empleado.salario * subidaAplicar / 100;
+        return empleado;
+    });
+}
+console.log(subirSalario(empleados2));
+console.log(empleados2);
+console.log("O mostrando sólo el nombre y salario:");
+empleados2.forEach(function (empleado) { return console.log("Nombre: ".concat(empleado.nombre, "  y  Nuevo Salario: ").concat(empleado.salario)); });
